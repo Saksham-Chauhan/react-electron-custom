b3131dff40bc89645794f2b14af65666a953f8d1
jack1998 <yaduvanshiabhi1998@gmail.com>
2022-03-02 12:30:22 +0530

    remove unnecessary files

--- src/pages-component/spoof-component/spoof-table-section/SpooferTable.js
+++ src/pages-component/spoof-component/spoof-table-section/SpooferTable.js
@@ -4,11 +4,11 @@ function SpooferTable() {
   return (
     <div className="spoofer-page-table-section">
       <div className="spooger-page-table-header">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>#</div>
+        <div>URL</div>
+        <div>Proxy</div>
+        <div>Status</div>
+        <div>Actions</div>
       </div>
     </div>
   );
