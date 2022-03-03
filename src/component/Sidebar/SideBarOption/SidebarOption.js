4cf5995836e01e1a2af9bbb092109ae4891a812d
pradeep <pradeep87055@gmail.com>
2022-03-03 17:51:01 +0530

    animation added

--- src/component/Sidebar/SideBarOption/SidebarOption.js
+++ src/component/Sidebar/SideBarOption/SidebarOption.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./SidebarOption.css";
 import { Link } from "react-router-dom";
 
+import { RoutePath } from "../../../constant";
+
 const SideBarOption = ({
   defaultIcon = "",
   activeIcon,
@@ -14,12 +16,57 @@ const SideBarOption = ({
     <Link
       {...props}
       to={pageTo}
-      className={`sidebarOption ${linkClassName} ${activeClassName}`}
+      className={`sidebarOption ${linkClassName} ${activeClassName} nav__item `}
     >
-      <img src={defaultIcon} alt="default-sidebar-icon" />
-      <img src={activeIcon} alt="active-sidebar-icon" />
+      {/* <img src={defaultIcon} alt="default-sidebar-icon" /> */}
+
+      {activeClassName ? (
+        <div className="activeimage">
+          <img src={activeIcon} alt="active-sidebar-icon" />
+        </div>
+      ) : (
+        <div
+          className={
+            pageTo === RoutePath.home
+              ? "home_animation "
+              : pageTo === RoutePath.oneclick
+              ? "OneClick_animation"
+              : pageTo === RoutePath.profile
+              ? "Profile_animation"
+              : pageTo === RoutePath.proxy
+              ? "Proxy_animation"
+              : pageTo === RoutePath.accountGen
+              ? "AccounGen_animation"
+              : pageTo === RoutePath.linkOpener
+              ? "linkOpner_animation"
+              : pageTo === RoutePath.inviteJoiner
+              ? "inviteJoiner_animation"
+              : pageTo === RoutePath.spoofer
+              ? "spoofer_animation"
+              : pageTo === RoutePath.twitter
+              ? "twitter_animation"
+              : pageTo === RoutePath.settings
+              ? "setting_animation"
+              : pageTo === "/appupdate"
+              ? "update_animation"
+              : ""
+          }
+        ></div>
+      )}
+
+      {/* <div className="OneClick_animation"></div> */}
     </Link>
   );
 };
 
 export default SideBarOption;
+
+// pageTo === "/proxy"
+//   ? "Proxy_animation"
+//   : pageTo === "/linkOpener"
+//   ? "Link_animation"
+//   : pageTo === "/oneclick"
+//   ? "oneclick_animation"
+//   : pageTo === "/account"
+//   ? "account_animation"
+//   : "";
