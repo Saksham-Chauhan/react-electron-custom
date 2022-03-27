92d66f8691efc89dd418388700f632d71970a7ae
jack1998 <yaduvanshiabhi1998@gmail.com>
2022-03-27 17:21:18 +0530

    fix coming soon modal title and system animation webhook issue

--- src/pages/miniting/Miniting.js
+++ src/pages/miniting/Miniting.js
@@ -5,7 +5,7 @@ import "./styles.css";
 const oneClickPage = () => {
   return (
     <div className="miniting-page">
-      <ComingSoon />
+      <ComingSoon modalTitle="NFT Minter" />
     </div>
   );
 };
