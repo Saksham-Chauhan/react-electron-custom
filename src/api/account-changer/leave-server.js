e04a3d55405f3f301e24cb9b757f48b36833b7cc
jack1998 <yaduvanshiabhi1998@gmail.com>
2022-03-28 17:31:02 +0530

    add try catch in acc changer api call and fix some bugs

--- src/api/account-changer/leave-server.js
+++ src/api/account-changer/leave-server.js
@@ -2,14 +2,21 @@ import axios from "axios";
 import { BASE_URL } from "../index";
 
 async function leaveServer(token, guildid) {
-  const json = JSON.stringify({ lurking: false });
+  try {
+    const json = JSON.stringify({ lurking: false });
 
-  let res = await axios.delete(`${BASE_URL}users/@me/guilds/${guildid}`, json, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `${token}`,
-    },
-  });
-  return res;
+    let res = await axios.delete(
+      `${BASE_URL}users/@me/guilds/${guildid}`,
+      json,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      }
+    );
+    return res;
+  } catch (error) {
+    throw new Error("Something went wrong on leaving server" + error.message);
+  }
 }
-leaveServer();
