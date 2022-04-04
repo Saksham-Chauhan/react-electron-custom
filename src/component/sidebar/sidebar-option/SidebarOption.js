b24a36a05a619550834d0b56d15461d79fbc63bd
Xhunter <kartikey@koders.in>
2022-04-04 12:59:47 +0530

    Fixed sidebar refactoring

---
+++ src/component/sidebar/sidebar-option/SidebarOption.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./styles.css";
+import { NavLink as Link } from "react-router-dom";
+
+const SideBarOption = ({
+  activeIcon,
+  defaultIcon,
+  pageTo,
+  activeClass,
+  isLink = true,
+  ...props
+}) => {
+  return isLink ? (
+    <Link
+      to={pageTo}
+      className={({ isActive }) =>
+        !isActive ? `sidebar-option ` : `sidebar-option active-link `
+      }
+    >
+      <div className={`sidebar-option-inner bg-animation-css  ${activeClass} `}>
+        <img src={defaultIcon} alt="active-sidebar-icon" />
+        <img src={activeIcon} alt="active-sidebar-icon" />
+        <div className="anim" />
+      </div>
+    </Link>
+  ) : (
+    <div {...props} className={`sidebar-option `}>
+      <div className={`sidebar-option-inner  ${activeClass} `}>
+        <img src={defaultIcon} alt="active-sidebar-icon" />
+        <img src={activeIcon} alt="active-sidebar-icon" />
+      </div>
+    </div>
+  );
+};
+
+export default SideBarOption;
